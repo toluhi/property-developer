404a60fc9c99883b11db82aa4499f2fc84e94405
yemi-t <toluhi@yahoo.com>
2018-05-07 14:00:00 +0100

    webpack babel react setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <p>Let's Go!</p>
+    </div>
+  );
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("app"));
